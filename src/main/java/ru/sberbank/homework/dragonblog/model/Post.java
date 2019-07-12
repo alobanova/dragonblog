e526bd926c932608e019dfcd9fdb5623f6756bb5
aloba <alobanova2144@gmail.com>
2019-07-12 13:58:37 +0300

    User and post entity

--- src/main/java/ru/sberbank/homework/dragonblog/model/Post.java
+++ src/main/java/ru/sberbank/homework/dragonblog/model/Post.java
@@ -3,28 +3,56 @@ package ru.sberbank.homework.dragonblog.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Mart
  * 30.06.2019
  **/
+@Entity
+@Table(name = "Post")
 @NoArgsConstructor
-public class Post extends AbstractNamedEntity {
-    @Getter @Setter
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Post_id_seq")
+    @SequenceGenerator(name = "Post_id_seq", sequenceName = "Post_id_seq")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_author")
     private User author;
 
-    @Getter @Setter
+    @Column(name = "date")
     private LocalDateTime postDateTime;
 
-    @Getter @Setter
+    @Column(name = "description")
     private String Description;
 
-    @Getter @Setter
-    private byte[] photo;
+    private byte[] photo = null;
 
-    @Getter @Setter
+    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
     private List<Comment> comments;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 }
