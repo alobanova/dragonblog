98592c63e57c753cb3a90aa131313ceff9e579b9
OLGGOR <gorskih.olka@yandex.ru>
2019-07-12 14:45:31 +0300

    Изменена сущность комментов

--- src/main/java/ru/sberbank/homework/dragonblog/repository/CommentRepository.java
+++ src/main/java/ru/sberbank/homework/dragonblog/repository/CommentRepository.java
@@ -1,18 +1,13 @@
 package ru.sberbank.homework.dragonblog.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import ru.sberbank.homework.dragonblog.model.Comment;
 
 import java.util.List;
 
-/**
- * Возможно имеет смысл все репозитории сделать через Spring Data Repository
- * пока не помню детали
- * */
-public interface CommentRepository {
-    Comment save(Comment comment);
+public interface CommentRepository extends CrudRepository<Comment, Long> {
 
-    // false if comment do not belong to author/ post author/ admin
-    boolean delete(long id, long userId);
+    boolean deleteByIdAndAuthorId(long id, long userId);
 
-    List<Comment> getAllByPost(long postId);
+    List<Comment> findAllByPostId(long postId);
 }
